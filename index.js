b33310d80f04f6b02766cbb70e40e929d6d70f3b
bovino <FalaSerio@2016>
2022-05-30 08:08:16 -0300

    cluster mode implemented

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const cluster = require('cluster');
+const process = require('process');
+const os = require('os');
 const cors = require('cors');
-const app = express();
-require('./src/Routes/index')(app);
-app.use(cors());
-app.use(express.json());
-app.listen(3333);
+const cpus = os.cpus;
+const numCpus = cpus().length;
+console.log(' CPU number is:' + numCpus);
+
+if(cluster.isMaster){
+  console.log(`Primary ${process.pid} is running!`);
+
+  // fork workers
+  for(let i=0; i < numCpus; i++){
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+
+} else {
+    const app = express();
+    require('./src/Routes/index')(app);
+    
+    app.use(cors());
+    app.use(express.json());
+    app.listen(3333);
+
+    console.log(` Process {process.pid} started!`);
+}
+
